6cee3913ed09d74fa36eccf79f9ed92ca5767e42
Monique Rodrigues <moniqueoliveira0104@gmail.com>
2018-12-14 12:25:18 -0200

    incluído as dependências como devDependencies

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Home from './Paginas/Home/Home'
-// import OQueOferecemos from './Paginas/OQueOferecemos/OQueOferecemos'
-// import Tabela from './componentes/Tabela/Tabela'
-// import Cabecalho from './componentes/Cabecalho/Cabecalho'
-// import Login from './Paginas/Login/Login'
-// import Conta from './Paginas/Conta/Conta'
+import OQueOferecemos from './Paginas/OQueOferecemos/OQueOferecemos'
+import Tabela from './componentes/Tabela/Tabela'
+import Cabecalho from './componentes/Cabecalho/Cabecalho'
+import Login from './Paginas/Login/Login'
+import Conta from './Paginas/Conta/Conta'
 import './index.scss'
 
 
@@ -16,12 +16,12 @@ import './index.scss'
 function App(){
     return(
         <Switch>
-            {/* <Route path = "/home" exact component={Home}/>
+            <Route path = "/home" exact component={Home}/>
             <Route path = "/" exact component={Home}/>
             <Route path ="/oQueOferecemos" component={OQueOferecemos}/>
             <Route path= "/tabela" component ={Tabela}/>
             <Route path="/login" component={Login} />
-            <Route path="/conta" component={Conta}/>           */}
+            <Route path="/conta" component={Conta}/>          
         </Switch>
     )
 }
